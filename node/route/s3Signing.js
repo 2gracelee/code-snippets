8e33a7d1e98a41845aa89adcb2088ec8dda6bb77
2gracelee <46877095+2gracelee@users.noreply.github.com>
2019-01-21 12:14:09 -0800

    Delete s3Signing.js

--- node/route/s3Signing.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const filesController = require('../controllers/files.controller')
-
-router.get('/get-signed-url', filesController.getSignedUrl);
-
-module.exports = router;
